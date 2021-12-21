09ce669e7eb44c821ae2c78b19e20641022bf44e
Sergey Saverchenko <saverchenko2002@gmail.com>
2021-12-21 16:42:57 +0300

    remove redundant createTask, add invocable and helper method

--- force-app/main/default/lwc/addTasksButtonComponent/addTasksButtonComponent.js
+++
@@ -1,9 +0,0 @@
-import { api, LightningElement } from "lwc";
-import addTasks from "@salesforce/apex/OpportunityTriggerHelper.addTasks";
-
-export default class AddTasksButtonComponent extends LightningElement {
-  @api recordId;
-  async handleClick() {
-    await addTasks({ oldOppsMap: null, updatedOppsMap: null, manualOppId: this.recordId });
-  }
-}
--- force-app/main/default/triggers/OpportunityTrigger.trigger
+++ force-app/main/default/triggers/OpportunityTrigger.trigger
@@ -1,7 +1,5 @@
 trigger OpportunityTrigger on Opportunity(before update) {
-  OpportunityTriggerHandler handler = new OpportunityTriggerHandler();
-
   if (Trigger.isBefore && Trigger.isUpdate) {
-    handler.onBeforeUpdate(Trigger.oldMap, Trigger.newMap);
+    OpportunityTriggerHandler.onBeforeUpdate(Trigger.oldMap, Trigger.newMap);
   }
 }
